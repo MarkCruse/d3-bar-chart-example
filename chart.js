79f666367539e8ccf869143cbcbb98000caca2f8
Mark Cruse <MarkCruse@users.noreply.github.com>
2022-08-17 17:16:55 -0400

    start of adding tooltip

--- chart.js
+++ chart.js
@@ -1,16 +1,15 @@
 import * as d3 from "d3";
 
 async function drawBars() {
-
   // 1. Access data
-  const dataset = await d3.json("./data/my_weather_data.json")
+  const dataset = await d3.json("./data/my_weather_data.json");
 
-  const xAccessor = d => d.humidity
-  const yAccessor = d => d.length
+  const xAccessor = (d) => d.humidity;
+  const yAccessor = (d) => d.length;
 
   // 2. Create chart dimensions
 
-  const width = 600
+  const width = 600;
   let dimensions = {
     width: width,
     height: width * 0.6,
@@ -18,69 +17,85 @@ async function drawBars() {
       top: 30,
       right: 10,
       bottom: 50,
-      left: 50,
-    },
-  }
-  dimensions.boundedWidth = dimensions.width
-    - dimensions.margin.left
-    - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height
-    - dimensions.margin.top
-    - dimensions.margin.bottom
+      left: 50
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
-  const wrapper = d3.select("#wrapper")
+  const wrapper = d3
+    .select("#wrapper")
     .append("svg")
-      .attr("width", dimensions.width)
-      .attr("height", dimensions.height)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
-  const bounds = wrapper.append("g")
-      .style("transform", `translate(${
-        dimensions.margin.left
-      }px, ${
-        dimensions.margin.top
-      }px)`)
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 
   // 4. Create scales
 
-  const xScale = d3.scaleLinear()
+  const xScale = d3
+    .scaleLinear()
     .domain(d3.extent(dataset, xAccessor))
     .range([0, dimensions.boundedWidth])
-    .nice()
+    .nice();
 
-  const binsGenerator = d3.bin()
+  const binsGenerator = d3
+    .bin()
     .domain(xScale.domain())
     .value(xAccessor)
-    .thresholds(12)
+    .thresholds(12);
 
-  const bins = binsGenerator(dataset)
+  const bins = binsGenerator(dataset);
 
-  const yScale = d3.scaleLinear()
+  const yScale = d3
+    .scaleLinear()
     .domain([0, d3.max(bins, yAccessor)])
     .range([dimensions.boundedHeight, 0])
-    .nice()
+    .nice();
 
   // 5. Draw data
+  // draw data
+  const tooltip = d3
+    .select("#wrapper")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-  const binsGroup = bounds.append("g")
+  const binsGroup = bounds.append("g");
 
-  const binGroups = binsGroup.selectAll("g")
-    .data(bins)
-    .join("g")
+  const binGroups = binsGroup.selectAll("g").data(bins).join("g");
 
-  const barPadding = 1
-  const barRects = binGroups.append("rect")
-      .attr("x", d => xScale(d.x0) + barPadding / 2)
-      .attr("y", d => yScale(yAccessor(d)))
-      .attr("width", d => d3.max([
-        0,
-        xScale(d.x1) - xScale(d.x0) - barPadding
-      ]))
-      .attr("height", d => dimensions.boundedHeight
-        - yScale(yAccessor(d))
+  const barPadding = 1;
+  const barRects = binGroups
+    .append("rect")
+    .attr("x", (d) => xScale(d.x0) + barPadding / 2)
+    .attr("y", (d) => yScale(yAccessor(d)))
+    .attr("width", (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
+    .attr("height", (d) => dimensions.boundedHeight - yScale(yAccessor(d)))
+    .attr("fill", "cornflowerblue");
+  barRects.on("mouseenter", (event, d) => {
+    const x = xScale(xAccessor(d));
+    const y = yScale(yAccessor(d));
+
+    // "translate(calc(10px - 50%), calc(10px - 100%))"
+    tooltip
+      .style(
+        "transform",
+        `translate(calc(${x + dimensions.margin.left}px - 50%), calc(${
+          y + dimensions.margin.top - 10
+        }px - 100%))`
       )
-      .attr("fill", "cornflowerblue")
-
+      .text(`Dew point: ${yAccessor(d)}`)
+      .style("opacity", 1);
+  });
 }
-drawBars()
+drawBars();
